ffeb281501120f54e23880f37406432ec47f6fc4
giulio <giulio@airhive.it>
2019-04-14 18:10:14 +0200

    Pacchetto GPS

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
