9bea9542e16856d1e1b44935718f165ee4e430a3
zanat <zanatta.davide703@gmail.com>
2019-04-14 18:57:29 +0200

    Aggiornato pubspec.yaml per maps

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -10,4 +10,16 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+// Add the GoogleMaps import.
+#import "GoogleMaps/GoogleMaps.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  // Add the following line with your API key.
+  [GMSServices provideAPIKey:@"AIzaSyCkWs0u3VdobkImHKj0xo8CI4bcqJ7EXHQ"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
 @end
