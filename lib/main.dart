b518b1ea5236889f359b192e23dae04f03a69e8a
giulio <giulio@airhive.it>
2019-04-22 18:58:43 +0200

    Mini correzione per checkout.

--- lib/main.dart
+++ lib/main.dart
@@ -279,8 +279,9 @@ class _AccountPage extends State<AccountPage> {
                       WebView(
                         onPageFinished: (ciao) => {setState((){
                           mostra_caricamento = false;
+                          print("https://www.airhive.it/account/?relog=true&tkn=$login_token");
                         })},
-                        initialUrl: "https://www.airhive.it/account?relog=true&tkn=$login_token",
+                        initialUrl: "https://www.airhive.it/account/index.php?relog=true&tkn=$login_token",
                         javascriptMode: JavascriptMode.unrestricted,
                       ),
                       mostra_caricamento ? CircularProgressIndicator() : Container(),
