2c419e8d3260647fff10db5e0a2cdc5fba28fadb
Andrea Carpi <andrea@andreacarpi.it>
2019-09-21 12:06:52 +0200

    fonts nuovi (?)

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ ThemeData app_theme(){
     textTheme: TextTheme(
       headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
       title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-      body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+      body1: TextStyle(fontSize: 14.0, fontFamily: 'PassionOne'),
     ),
   );
 }
